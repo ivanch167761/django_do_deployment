8fe4914af91ddb238b97e981d368850b580aaaa2
IK <iva.kulikov@gmail.com>
2022-10-31 14:46:06 +0100

    add working test

--- pro_store/toTest/tests.py
+++ pro_store/toTest/tests.py
@@ -8,7 +8,7 @@ class mainTest(TestCase):
         ENV_SECRET_KEY = str(os.environ.get("DJANGO_SECRET_KEY"))
 
         print(ENV_SECRET_KEY)
-        self.assertEqual(settings.SECRET_KEY, 'abcdefg!!A')
+        self.assertEqual(settings.SECRET_KEY, 'abcdefg!!!')
     def test_envTestPass(self):
 
         ENV_SECRET_KEY = str(os.environ.get("DJANGO_SECRET_KEY"))

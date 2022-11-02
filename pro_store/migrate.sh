940a7920db3440aee367054b333a6b6c231b2d09
IK <iva.kulikov@gmail.com>
2022-11-02 04:22:17 +0100

    makemigrations

--- pro_store/migrate.sh
+++ pro_store/migrate.sh
@@ -4,5 +4,6 @@
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"IVA.KULIKOV@GMAIL.COM"}
 cd /app/
 
+/opt/venv/bin/python manage.py makemigrations --noinput
 /opt/venv/bin/python manage.py migrate --noinput
 /opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true

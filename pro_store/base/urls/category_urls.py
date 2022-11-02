bce5cbbfe117d7e45b89c840b52c19d3f067f310
IK <iva.kulikov@gmail.com>
2022-11-02 03:02:57 +0100

    add base backend

--- pro_store/base/urls/category_urls.py
+++ pro_store/base/urls/category_urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from base.views import category_views as views
 urlpatterns = [
     path('<str:cpk>/', views.getCategoryProducts, name='category'),

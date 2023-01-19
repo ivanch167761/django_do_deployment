a45574a699ca2a7089aeb3d75b16aa2d9c8a1472
ik <iva.kulikov@gmail.com>
2023-01-19 10:07:09 +0100

    fix users update url

--- pro_store/base/urls/user_urls.py
+++ pro_store/base/urls/user_urls.py
@@ -3,7 +3,7 @@ from base.views import user_views as views
 
 urlpatterns = [
     path('profile/', views.getUserProfile, name='users-profile'),
-    path('profile/update', views.updateUserProfile, name='update-users-profile'),
+    path('profile/update/', views.updateUserProfile, name='update-users-profile'),
     path('', views.getUsers, name='users'),
     path('register/', views.registerUser, name='register'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),

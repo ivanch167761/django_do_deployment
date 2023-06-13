d1d4ffbf9225248e0679ca82a565848c82f5bc50
ik <iva.kulikov@gmail.com>
2023-06-13 10:54:08 +0000

    add getAdminOrders

--- pro_store/base/views/order_view.py
+++ pro_store/base/views/order_view.py
@@ -87,6 +87,15 @@ def getMyOrders(request):
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
 
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
 @api_view(['PUT'])
 @permission_classes([IsAdminUser])
 def udateOrderToDelivered(request, pk):
